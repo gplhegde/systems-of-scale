8203ca7655c1e80fb6347fb97aed78e7cd927ab1
Gopal Hegde <gplhegde@gmail.com>
2026-08-03 10:10:50 +0800

    reset kafka

--- mod-02-kafka/fastcommerce/fastcommerce/settings/development.py
+++
@@ -1,9 +0,0 @@
-from .base import *
-
-DEBUG = True
-
-ALLOWED_HOSTS = ["*"]
-
-REST_FRAMEWORK["DEFAULT_RENDERER_CLASSES"].append(
-    "rest_framework.renderers.BrowsableAPIRenderer"
-)
--- mod-02-kafka/fastcommerce/shop/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class ShopConfig(AppConfig):
-    name = "shop"
-
-    default_auto_field = "django.db.models.BigAutoField"
